6f8dfac03a23b0d07c00a4b696d41fa99c3c0781
cielotds <tadascielo01@gmail.com>
2025-12-12 17:39:36 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  // THIS IS THE MOST IMPORTANT LINE!
+  base: '/age-calculator/', // Change this to your actual repo name
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
