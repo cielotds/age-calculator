6a1754da1d8a3fa32c3200f17665bbe419638b44
cielotds <tadascielo01@gmail.com>
2025-12-12 18:09:35 +0800

    react

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  // THIS IS THE MOST IMPORTANT LINE!
-  base: '/age-calculator/', // Change this to your actual repo name
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
+});
